5993bf90ad725596a9a75992cd4b45e2dfd46aaa
Martin Schimandl <martin.schimandl@gmail.com>
2017-07-26 12:38:45 +0200

    Update Gridcoin cask to 3.5.9.7

--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,10 +1,10 @@
 cask 'gridcoin' do
-  version '3.5.9.5'
-  sha256 'e5074e0ad91cab272b4bd102cfacd960964a61cbeef044d698b4d979d9238129'
+  version '3.5.9.7'
+  sha256 '1d957c5533660c6b83bc3371cc854b205bd0af136959abc94e86c5300f9537d2'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/#{version}/gridcoinresearch.dmg"
   appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom',
-          checkpoint: 'e8b83306fe17419156b021768620967f9a1d438aa6b9e66d8a8457b5aebf830b'
+          checkpoint: 'c0e57286541f39b55ba8d008c72bde187f53d8fc9ee109eed03a8bc8f5365991'
   name 'Gridcoin macOS Wallet'
   homepage 'https://gridcoin.us/'
 
