e6a48f29cadfab8e8fd08e2f225e320dbba5cb16
Martin Schimandl <martin.schimandl@gmail.com>
2018-09-05 08:08:07 +0200

    Update Cask to 3.7.16.0 and remove appcast checkpoint

--- .scripts/updateSHAsums.sh
+++ .scripts/updateSHAsums.sh
@@ -8,9 +8,8 @@ if [[ -z  $version ]]; then
   exit -1
 fi
 
+wget -P ~/Downloads  https://github.com/Git-Jiro/homebrew-jiro/releases/download/$version/gridcoinresearch.dmg
 filesha=`shasum -a 256 ~/Downloads/gridcoinresearch.dmg | cut -d' ' -f 1`
-appcastsha=`brew cask _appcast_checkpoint --calculate "https://github.com/Git-Jiro/homebrew-jiro/releases.atom"`
 
 sed -e "s/version '.*'/version '$version'/"            -i '' ../Casks/gridcoin.rb
 sed -e "s/sha256 '.*'/sha256 '$filesha'/"              -i '' ../Casks/gridcoin.rb
-sed -e "s/checkpoint: '.*'/checkpoint: '$appcastsha'/" -i '' ../Casks/gridcoin.rb
--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,10 +1,9 @@
 cask 'gridcoin' do
-  version '3.7.15.0'
-  sha256 '73ab392ce6db001032b76a7976d83d86a90c41981500cc2b5d6e0d87d04654ee'
+  version '3.7.16.0'
+  sha256 '2811a63794e7cb377c7aa8a0864b918aa08a1efd4474cdc611b7fb07ea61ebe5'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/#{version}/gridcoinresearch.dmg"
-  appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom',
-          checkpoint: '017ee0b6072c0098901a779a029564211deac1b65d0f39e8d087f5cac1f5f9ef'
+  appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom'
   name 'Gridcoin macOS Wallet'
   homepage 'https://gridcoin.us/'
 
