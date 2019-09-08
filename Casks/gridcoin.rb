217339ed1588b195748c323cde43d400f3366be1
Martin Schimandl <martin.schimandl@gmail.com>
2019-09-08 15:02:56 +0200

    Update to 4.0.5.0 Leisure

--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,6 +1,6 @@
 cask 'gridcoin' do
-  version '4.0.4.0'
-  sha256 '010eebb96e28155ddae280fcded543d9456dbf4f6e7b65784170dc82ecc9fd0e'
+  version '4.0.5.0'
+  sha256 'd7155325a07022363b265c3a468d486c9b62a424697a0407d229ac89a073b610'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/#{version}/gridcoinresearch.dmg"
   appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom'
