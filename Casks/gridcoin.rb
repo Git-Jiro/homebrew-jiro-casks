5f6fba485c61663114e296f0550cbb3a96071d3c
Martin Schimandl <martin.schimandl@gmail.com>
2020-06-07 12:28:11 +0200

    fix checksum for 4.0.6.0 leasure

--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,6 +1,6 @@
 cask 'gridcoin' do
   version '4.0.6.0'
-  sha256 '48002b867661c85b2cbb41d897d6821cd39d58dfff404832ecbbafbe7eaf2569'
+  sha256 '3df3e7dd200f43e8d64b45e60312978370bc803fceedb7451fc401c8483dbf48'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/#{version}/gridcoinresearch.dmg"
   appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom'
