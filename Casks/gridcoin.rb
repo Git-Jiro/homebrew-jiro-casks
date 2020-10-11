7c2948ecf7cbf5a4da512f2a2347e93f62eb161c
Martin Schimandl <martin.schimandl@gmail.com>
2020-10-11 15:49:25 +0200

    Update to 5.0.2.0

--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,6 +1,6 @@
 cask 'gridcoin' do
-  version '5.0.0.0'
-  sha256 '9ed94168932ac508d0915d948ea9f40f8c83c90ee069b097ac16ed6e0829f06d'
+  version '5.0.2.0'
+  sha256 '9f23dfd3a78f5370461be4e59a8c30b38e2b67631de1b4ee28f5406bd23db7d7'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/#{version}/gridcoinresearch.dmg"
   appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom'
