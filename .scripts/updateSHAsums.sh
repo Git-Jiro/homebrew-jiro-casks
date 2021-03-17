ed2a699d1cc2aa3cd94ededb82a3ef3e216c526a
Martin Schimandl <martin.schimandl@gmail.com>
2021-03-17 16:48:16 +0100

    Update to 5.3.0.0 Mandatory

--- .scripts/updateSHAsums.sh
+++ .scripts/updateSHAsums.sh
@@ -9,7 +9,7 @@ if [[ -z  $version ]]; then
 fi
 
 wget -P ~/Downloads  https://github.com/Git-Jiro/homebrew-jiro/releases/download/v$version/gridcoinresearch_bigsur.dmg
-filesha=`shasum -a 256 ~/Downloads/gridcoinresearch.dmg | cut -d' ' -f 1`
+filesha=`shasum -a 256 ~/Downloads/gridcoinresearch_bigsur.dmg | cut -d' ' -f 1`
 
 sed -e "s/version '.*'/version '$version'/"            -i '' ../Casks/gridcoin.rb
 sed -e "s/sha256 '.*'/sha256 '$filesha'/"              -i '' ../Casks/gridcoin.rb
--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,6 +1,6 @@
 cask 'gridcoin' do
-  version '5.2.0.0'
-  sha256 ''
+  version '5.3.0.0'
+  sha256 'a0deae50a7f75d32352f6b5edef504b78222ab1abf955187b801d197c9009f03'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/v#{version}/gridcoinresearch_bigsur.dmg"
   appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom'
