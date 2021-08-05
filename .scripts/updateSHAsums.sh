98a4a56b032af60233c47b37bec5d7a1125f2f0e
Martin Schimandl <martin.schimandl@gmail.com>
2021-08-05 19:42:38 +0200

    Update to 5.3.2.0

--- .scripts/updateSHAsums.sh
+++ .scripts/updateSHAsums.sh
@@ -8,8 +8,8 @@ if [[ -z  $version ]]; then
   exit -1
 fi
 
-wget -P ~/Downloads  https://github.com/Git-Jiro/homebrew-jiro/releases/download/v$version/gridcoinresearch_bigsur.dmg
-filesha=`shasum -a 256 ~/Downloads/gridcoinresearch_bigsur.dmg | cut -d' ' -f 1`
+wget -P ~/Downloads  https://github.com/Git-Jiro/homebrew-jiro/releases/download/v$version/gridcoinresearch_bigsur_or_mojave.dmg
+filesha=`shasum -a 256 ~/Downloads/gridcoinresearch_bigsur_or_mojave.dmg | cut -d' ' -f 1`
 
 sed -e "s/version '.*'/version '$version'/"            -i '' ../Casks/gridcoin.rb
 sed -e "s/sha256 '.*'/sha256 '$filesha'/"              -i '' ../Casks/gridcoin.rb
--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,11 +1,11 @@
 cask 'gridcoin' do
-  version '5.3.0.0'
-  sha256 'a0deae50a7f75d32352f6b5edef504b78222ab1abf955187b801d197c9009f03'
+  version '5.3.2.0'
+  sha256 '044c20f326798b0efe273c793d2cf15a1b5985130a8172b4c6072e79c5391102'
 
-  url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/v#{version}/gridcoinresearch_bigsur.dmg"
+  url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/v#{version}/gridcoinresearch_bigsur_or_mojave.dmg"
   appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom'
   name 'Gridcoin macOS Wallet'
   homepage 'https://gridcoin.us/'
 
-  app 'gridcoinresearch.app'
+  app 'Gridcoin.app'
 end
