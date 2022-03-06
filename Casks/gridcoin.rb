46add5876309736f3d3db20a4b94ae321f9726b6
Martin Schimandl <martin.schimandl@gmail.com>
2022-03-06 19:26:55 +0000

    Update to version 5.3.3.0

--- Casks/gridcoin.rb
+++ Casks/gridcoin.rb
@@ -1,6 +1,6 @@
 cask 'gridcoin' do
-  version '5.3.2.0'
-  sha256 '044c20f326798b0efe273c793d2cf15a1b5985130a8172b4c6072e79c5391102'
+  version '5.3.3.0'
+  sha256 '6978e5b71b6a247e55229fa8d737c8e2314cfa4e186315d0371e1176af16e548'
 
   url "https://github.com/Git-Jiro/homebrew-jiro/releases/download/v#{version}/gridcoinresearch_bigsur_or_mojave.dmg"
   appcast 'https://github.com/Git-Jiro/homebrew-jiro/releases.atom'
